912808eb2596ed8385c80043cdc8e4559d1624c8
Dzianis Shapel <dzianis283@gmail.com>
2023-05-01 08:06:26 +0300

    selenide well done

--- src/test/java/OrderProductTest.java
+++ src/test/java/OrderProductTest.java
@@ -1,14 +1,8 @@
+
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import pages.CheckoutStepTwoPage;
 import pages.HomePage;
 import pages.InventoryPage;
 
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("User flow for ordering product")
