87bb190b2c8906f15e872438cad8e38d1f282e9e
Dzianis Shapel <dzianis283@gmail.com>
2023-05-01 20:00:14 +0300

    123

--- src/test/java/RemoveProductFromCartTest.java
+++ src/test/java/RemoveProductFromCartTest.java
@@ -1,20 +1,7 @@
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.HomePage;
 import pages.InventoryPage;
 
-import java.time.Duration;
-import java.util.*;
-import java.util.stream.Collectors;
-
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("Check correct removing item from cart")
